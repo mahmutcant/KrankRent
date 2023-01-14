c3582399798a33724e6cc12bf89c5cdab4e1d6a1
mahmutcant <mahcan012@gmail.com>
2023-01-14 17:59:24 +0300

    Takip Eden kullanıcılara bildirim gönderildi

---
+++ app/src/main/java/com/mahmut/krankrent/AdminSpecialPage.java
@@ -0,0 +1,14 @@
+package com.mahmut.krankrent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminSpecialPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_special_page);
+    }
+}
